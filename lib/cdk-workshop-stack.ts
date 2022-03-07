d1da44099404cd23317d13bfdae7e35ed8fa3631
Tomer Arbel Eliyahu <tomer.b.eliyahu@intel.com>
2022-03-07 21:42:05 +0200

    add hello lambda
    Signed-off-by: Tomer Arbel Eliyahu <tomer.b.eliyahu@intel.com>

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // nothing here!
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'), // relative path from where we run `cdk`
+      handler: 'hello.handler' // file is 'hello', function is 'handler'
+    });
   }
 }
